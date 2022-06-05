95a017d44ef6689f0f6635552b57dbc8e4afc560
jamesacox53 <jamesacox53@gmail.com>
2022-06-05 17:14:53 +0100

    Completed Exercise- FizzBuzz

---
+++ TestNinja.UnitTests/Fundamentals/FizzBuzzTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    { 
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(-30, "FizzBuzz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(-6, "Fizz")]
+        [TestCase(20, "Buzz")]
+        [TestCase(-40, "Buzz")]
+        [TestCase(26, "26")]
+        [TestCase(-11, "-11")]
+        public void GetOutput_NumberEntered_ReturnsCorrectString(int input, string expectedResult)
+        {
+            string result = FizzBuzz.GetOutput(input);
+
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+
+
+    }
+}
