e75425f612b537d81cbc4010a1fa10f5a77c5444
jamesacox53 <jamesacox53@gmail.com>
2022-06-05 14:50:13 +0100

    Completed Testing Strings

---
+++ TestNinja.UnitTests/Fundamentals/HTMLFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class HTMLFormatterTests
+    {
+        private HtmlFormatter htmlFormatter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            htmlFormatter = new HtmlFormatter();
+        }
+        
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            string res = htmlFormatter.FormatAsBold("abc123");
+
+            Assert.IsTrue(res == "<strong>abc123</strong>");
+        }
+    }
+}
