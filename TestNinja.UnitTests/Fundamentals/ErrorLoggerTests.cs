e6a61b69ce1f88e510387c88f314a5dfd3f1f7d1
jamesacox53 <jamesacox53@gmail.com>
2022-06-05 16:03:54 +0100

    Completed Testing Void Methods

---
+++ TestNinja.UnitTests/Fundamentals/ErrorLoggerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Fundamentals
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger logger;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            logger.Log("a");
+
+            Assert.IsTrue((logger.LastError) == "a");
+        }
+    }
+}
